1fcf4c6611d31e45bcec98cb8a476af79be24173
Eddie Poon <epoon0921@bths.edu>
2025-04-10 14:39:30 -0400

    Update for unclear error

--- notebooks/meaniou.py
+++ notebooks/meaniou.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
+import keras
 
+#@keras.saving.register_keras_serializable()
 class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
     def __init__(self,
                y_true=None,
                y_pred=None,
                num_classes=None,
                name=None,
-               dtype=None):
+               dtype=None,
+               ignore_class=None):
         super(UpdatedMeanIoU, self).__init__(num_classes = num_classes,name=name, dtype=dtype)
 
     def update_state(self, y_true, y_pred, sample_weight=None):
