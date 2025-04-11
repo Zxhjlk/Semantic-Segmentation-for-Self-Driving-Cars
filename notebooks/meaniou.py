894a7b29be81bd030a5b1a5517aa918db18f5fe2
Eddie Poon <epoon0921@bths.edu>
2025-04-10 20:29:36 -0400

    Attempt model save/load issue

--- notebooks/meaniou.py
+++ notebooks/meaniou.py
@@ -9,9 +9,11 @@ class UpdatedMeanIoU(tf.keras.metrics.MeanIoU):
                num_classes=None,
                name=None,
                dtype=None,
-               ignore_class=None):
-        super(UpdatedMeanIoU, self).__init__(num_classes = num_classes,name=name, dtype=dtype)
-        self.ignore_class=ignore_class
+               ignore_class=None,
+               sparse_y_true=True,
+               sparse_y_pred=True,
+               axis=-1):
+        super(UpdatedMeanIoU, self).__init__(num_classes = num_classes,name=name, dtype=dtype, ignore_class=ignore_class, sparse_y_true=sparse_y_true, sparse_y_pred=sparse_y_pred, axis=axis)
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         y_pred = tf.math.argmax(y_pred, axis=-1)
